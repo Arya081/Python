f33486275d0187d93b14b966b60b25e3eb55a7cd
Arya081 <24BCH081@spt.pdpu.ac.in>
2025-02-08 10:46:46 +0530

    Create avgmarks.py

---
+++ avgmarks.py
@@ -0,0 +1,6 @@
+eng=eval(input("enter your english marks"))
+math=eval(input("enter your math marks"))
+sci=eval(input("enter your science marks"))
+tot=eng+math+sci
+avg=tot%3
+print(" average marks are ",avg)
