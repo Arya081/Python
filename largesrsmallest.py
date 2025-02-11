d6ae5591699c734ec72f41e561ea146631ae3f22
Arya081 <24BCH081@spt.pdpu.ac.in>
2025-02-11 15:23:08 +0530

    Create largesrsmallest.py

---
+++ largesrsmallest.py
@@ -0,0 +1,8 @@
+a=eval(input("enter 1st value"))
+b=eval(input("enter 2nd value"))
+if a>b:
+  print("largest is",a)
+  print("smallest is",b)
+else:
+  print("largest is",b)
+  print("smallest is",a)
