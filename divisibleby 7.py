06a4a3b79f2391b6a1d44775caa9ab4bf8825930
Arya Patel <24BCH081@spt.pdpu.ac.in>
2025-03-17 21:26:46 +0530

    Create divisibleby 7.py

---
+++ divisibleby 7.py
@@ -0,0 +1,19 @@
+a=eval(input("enter the number "))
+
+
+if a%7==0:
+    
+    print(a,"is divisible by 7")
+else:
+     print(a,"is not divisible by 7")
+
+
+
+
+
+output
+
+enter the number 67
+67 is not divisible by 7
+
+=== Code Execution Successful ===
