01dd2a9660805998c891f9eb38807d4eedd61184
Arya Patel <24BCH081@spt.pdpu.ac.in>
2025-03-17 21:20:06 +0530

    Create greater num.py

---
+++ greater num.py
@@ -0,0 +1,19 @@
+a=eval(input("enter the number "))
+b=eval(input("enter the number "))
+
+
+if a>b:
+    
+    print(a,"is greater the ",b)
+else:
+    print(b,"is greater the ",a)
+
+
+
+
+
+output
+
+enter the number 3
+enter the number 6
+6 is greater the  3
