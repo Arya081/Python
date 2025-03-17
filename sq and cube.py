234bcf1a25316bdcf9e9d4fb35177e5baa134b16
Arya Patel <24BCH081@spt.pdpu.ac.in>
2025-03-17 21:12:15 +0530

    Create sq and cube.py

---
+++ sq and cube.py
@@ -0,0 +1,19 @@
+num=eval(input("enter the number "))
+sq=0
+cube=0
+if num>=0:
+    sq=num**2
+    cube=num**3
+    print("Square of ",num,"is",sq)
+    print("cube of ",num,"is",cube)
+
+
+
+
+
+output
+
+enter the number 3
+Square of  3 is 9
+cube of  3 is 27
+
