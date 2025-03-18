5a9e69ca90e031ee5f6f3fa693ab69701673b77f
Arya Patel <24BCH081@spt.pdpu.ac.in>
2025-03-18 16:00:32 +0530

    Create valid triangle.py

---
+++ valid triangle.py
@@ -0,0 +1,20 @@
+a=eval(input("enter 1st angle"))
+b=eval(input("enter 2nd angle"))
+c=eval(input("enter 3rd angle"))
+sum=a+b+c
+
+if sum==180:
+  print(sum,"is valid triangle ")
+
+else:
+  print(sum,"is not a valid triangle")
+
+
+  
+
+output
+
+enter 1st angle20
+enter 2nd angle108
+enter 3rd angle52
+180 is valid triangle 
