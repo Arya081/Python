25133a7c00256d0d3506e0b25214b22389b141da
Arya Patel <24BCH081@spt.pdpu.ac.in>
2025-03-18 15:50:35 +0530

    Create major and minor.py

---
+++ major and minor.py
@@ -0,0 +1,15 @@
+a=eval(input("enter age"))
+
+
+if a>18:
+  print(a," age comes under major ")
+
+else:
+  print(a," age comes under minor ")
+
+  
+
+output
+
+enter age50
+50  age comes under major 
