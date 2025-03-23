c8fb37f39d4dee6ef5918c3755eebc046b6b9883
Arya Patel <24BCH081@spt.pdpu.ac.in>
2025-03-23 17:24:54 +0530

    Create fahrenheit to celcius.py

---
+++ fahrenheit to celcius.py
@@ -0,0 +1,17 @@
+import random
+f=[32,100,50]
+c=[]
+for i in f:
+    celcius=((i-32)*5)/9
+    c.append(celcius)
+
+print('tempreture in fahrenheit is ',f)
+print('List of tempreture in celcius is ',c)
+
+
+output
+
+tempreture in fahrenheit is  [32, 100, 50]
+List of tempreture in celcius is  [0.0, 37.77777777777778, 10.0]
+
+=== Code Execution Successful ===
