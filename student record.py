fd1b5e17bfc0146b325dab501aad422df526520c
Arya Patel <24BCH081@spt.pdpu.ac.in>
2025-03-23 22:08:20 +0530

    Create student record.py

---
+++ student record.py
@@ -0,0 +1,21 @@
+l=[(1,'arya',18),(2,'riya',19),(3,'yesha',18)]
+roll=[]
+age=[]
+name=[]
+
+for i in l:
+    roll.append(i[0])
+    age.append(i[2])
+    name.append(i[1])
+print('roll no: ',roll)
+print('name:',name)
+print('age:',age)
+
+
+output
+
+roll no:  [1, 2, 3]
+name: ['arya', 'riya', 'yesha']
+age: [18, 19, 18]
+
+=== Code Execution Successful ===
