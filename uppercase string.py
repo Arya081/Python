ee1cb57540da5358f0787f68883ce48a74a2945e
Arya Patel <24BCH081@spt.pdpu.ac.in>
2025-03-23 17:20:07 +0530

    Create uppercase string.py

---
+++ uppercase string.py
@@ -0,0 +1,15 @@
+import random
+l=['toy','chair','ball','table','bat']
+new=[]
+for i in l:
+    n=i.upper()
+    new.append(n)
+
+print(l)
+print('List of uppercase string is ',new)
+
+
+output
+
+['toy', 'chair', 'ball', 'table', 'bat']
+List of uppercase string is  ['TOY', 'CHAIR', 'BALL', 'TABLE', 'BAT']
