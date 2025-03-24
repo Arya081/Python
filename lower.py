0df137bc629d8cc74f7fe4fc405856d2ab8fa24c
Arya Patel <24BCH081@spt.pdpu.ac.in>
2025-03-24 19:08:36 +0530

    Create lower.py

---
+++ lower.py
@@ -0,0 +1,16 @@
+def lower(str):
+    for i in str:
+        str=chr(ord(i)+32)
+        print(str,end='')
+    
+    
+str=input('enter string in uppercase ')
+lower(str)
+
+
+
+output
+
+enter string in uppercase PDEU
+pdeu
+=== Code Execution Successful ===
