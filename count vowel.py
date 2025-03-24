6a911f999eaf95a9b0eb878ffadef3964967571e
Arya Patel <24BCH081@spt.pdpu.ac.in>
2025-03-24 18:26:52 +0530

    Create count vowel.py

---
+++ count vowel.py
@@ -0,0 +1,16 @@
+str=input('enter the string ')
+v=0
+for i in str:
+    if(i=='a' or i=='e' or i=='o' or i=='u'or i=='i'):
+        v=v+1
+        
+print('number of vowels are:',v)
+
+
+output
+
+enter the string arya
+number of vowels are: 2
+
+=== Code Execution Successful ===
+    
