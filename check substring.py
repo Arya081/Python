bc6abe91bfe29c3332570325d099d6bcaea1ffc0
Arya Patel <24BCH081@spt.pdpu.ac.in>
2025-03-24 18:32:40 +0530

    Create check substring.py

---
+++ check substring.py
@@ -0,0 +1,16 @@
+a=input('enter the string 1 ')
+b=input('enter the string 2 ')
+if a.find(b)!=0:
+    print(b,'is present in ',a)
+else:
+    print(b,'is not present in',a)
+
+        
+
+output
+
+enter the string 1 arya
+enter the string 2 ya
+ya is present in  arya
+
+=== Code Execution Successful ===
