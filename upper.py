7e1569d94a394171b6fa3d3de0d8b42e3f42f8c9
Arya Patel <24BCH081@spt.pdpu.ac.in>
2025-03-24 19:09:45 +0530

    Create upper.py

---
+++ upper.py
@@ -0,0 +1,14 @@
+def upper(str):
+    for i in str:
+        str=chr(ord(i)-32)
+        print(str,end='')
+    
+    
+str=input('enter string in lowercase ')
+upper(str)
+
+
+output
+enter string in lowercase pdeu
+PDEU
+=== Code Execution Successful ===
