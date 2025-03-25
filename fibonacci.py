11f6041775cef9b22ba3fa46b6ea5994dba6d5b0
Arya Patel <24BCH081@spt.pdpu.ac.in>
2025-03-25 15:30:39 +0530

    Create fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,13 @@
+def fibo():
+    n=eval(input('enter number '))
+    a=0
+    for i in range(1,n+1):
+        a=a+i
+    print('fibonacci of ',n,'is',a)
+fibo()
+
+
+output
+enter number 5
+fibonacci of  5 is 15
+>>> 
