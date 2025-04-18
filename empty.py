ae614fd6c9484abb5662a30266edcbb9b2e5269c
Arya Patel <24BCH081@spt.pdpu.ac.in>
2025-04-19 00:01:59 +0530

    Create empty.py

---
+++ empty.py
@@ -0,0 +1,24 @@
+d={}
+d1={1:'one'}
+print('d = ',d)
+print('d1 = ',d1)
+if len(d)==0:
+    print('D Dictionary is empty')
+else:
+    print('D Dictionary is not empty')
+if len(d1)==0:
+    print('D1 Dictionary is empty')
+else:
+    print('D1 Dictionary is not empty')
+
+
+
+
+output
+
+d =  {}
+d1 =  {1: 'one'}
+D Dictionary is empty
+D1 Dictionary is not empty
+
+=== Code Execution Successful ===
