ae3b2cec5e58d2bcdc60c8800b2ed692d2b668e5
Arya Patel <24BCH081@spt.pdpu.ac.in>
2025-04-18 23:31:26 +0530

    Create concate.py

---
+++ concate.py
@@ -0,0 +1,10 @@
+d1={1:'One',2:'Two',3:'Three'}
+d2={4:'Four',5:'Five',6:'Six'}
+d3={7:'Seven',8:'Eight',9:'Nine'}
+d4={**d1,**d2,**d3}
+print(d4)
+
+
+
+output
+{1: 'One', 2: 'Two', 3: 'Three', 4: 'Four', 5: 'Five', 6: 'Six', 7: 'Seven', 8: 'Eight', 9: 'Nine'}
