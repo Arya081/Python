3a482f9922eaf4b5e7476faf31097e663a68bb2e
Arya Patel <24BCH081@spt.pdpu.ac.in>
2025-04-19 23:21:13 +0530

    Create count_alpha_digits().py

---
+++ count_alpha_digits().py
@@ -0,0 +1,21 @@
+def count_alpha_digits():
+    s=0
+    c=0
+    for x in str:
+        if x.isalpha():
+            s=s+1
+        elif x.isdigit():
+            c=c+1
+    print('alphabets are : ',s,'\ndigits are: ',c)
+
+str=input('enter string: ')
+count_alpha_digits()
+
+
+output
+
+enter string: arya is 18
+alphabets are :  6 
+digits are:  2
+
+=== Code Execution Successful ===
