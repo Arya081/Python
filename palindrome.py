b5e10fde7d1b8a4ecb7353c4225abef1875eaf22
Arya Patel <24BCH081@spt.pdpu.ac.in>
2025-04-19 23:05:35 +0530

    Create palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,16 @@
+def ispalindrome(s):
+    if(s==s[::-1]):
+        print('string is palindrome')
+    else:
+        print('string is not palindrome')
+
+s=input('enter string ')
+ispalindrome(s)
+
+
+
+output
+enter string bob
+string is palindrome
+
+=== Code Execution Successful ===
