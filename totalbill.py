5661fcf79a410b9ab7a1f7c0676150bbe35d2d1e
Arya Patel <24BCH081@spt.pdpu.ac.in>
2025-04-19 12:11:34 +0530

    Create totalbill.py

---
+++ totalbill.py
@@ -0,0 +1,13 @@
+groc={'apple':20,'soap':40,'tub':100}
+qua={'apple':5,'soap':2,'tub':1}
+tot=0
+for i in groc:
+    tot=tot+(groc[i]*qua[i])
+total={tot}
+print(total)
+
+
+output
+{280}
+
+=== Code Execution Successful ===
