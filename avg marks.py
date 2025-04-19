bdc9f6c815e80954b21f094b8d3aae4e7d503bdf
Arya Patel <24BCH081@spt.pdpu.ac.in>
2025-04-19 14:21:52 +0530

    Create avg marks.py

---
+++ avg marks.py
@@ -0,0 +1,24 @@
+def sum_avg():
+    a=eval(input('enter marks of english '))
+    b=eval(input('enter marks of math '))
+    c=eval(input('enter marks of ssc '))
+    d=eval(input('enter marks of science'))
+    e=eval(input('enter marks of computer '))
+    sum=a+b+c+d+e
+    avg=sum/5
+    print('sum is ',sum,'\n average is ',avg)
+    
+sum_avg()
+
+
+output
+
+enter marks of english 18
+enter marks of math 19
+enter marks of ssc 19
+enter marks of science20
+enter marks of computer 20
+sum is  96 
+ average is  19.2
+
+=== Code Execution Successful ===
