31abe285669a29d7079c5004fd86ca53e7be5cd6
Arya Patel <24BCH081@spt.pdpu.ac.in>
2025-04-19 14:50:28 +0530

    Create list of tupple.py

---
+++ list of tupple.py
@@ -0,0 +1,11 @@
+def tup(n):
+    return[(x,x**2,x**3) for x in range(1,n+1)]
+
+print(tup(5))
+
+
+output
+
+[(1, 1, 1), (2, 4, 8), (3, 9, 27), (4, 16, 64), (5, 25, 125)]
+
+=== Code Execution Successful ===
