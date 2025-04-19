15804c511b9b0fa826fb3682c05252e7f4f2c397
Arya Patel <24BCH081@spt.pdpu.ac.in>
2025-04-19 23:24:34 +0530

    Update count_alpha_digits().py

--- count_alpha_digits().py
+++ count_alpha_digits().py
@@ -6,7 +6,7 @@ def count_alpha_digits():
             s=s+1
         elif x.isdigit():
             c=c+1
-    print('alphabets are : ',s,'\ndigits are: ',c)
+    print({"alphabets are":s,"digits are":c})
 
 str=input('enter string: ')
 count_alpha_digits()
@@ -15,7 +15,6 @@ count_alpha_digits()
 output
 
 enter string: arya is 18
-alphabets are :  6 
-digits are:  2
+{'alphabets are': 6, 'digits are': 2}
 
 === Code Execution Successful ===
