e0b95916af2a4d9a9274d61cc8d0c52d31ecc77c
Arya Patel <24BCH081@spt.pdpu.ac.in>
2025-05-01 16:28:23 +0530

    Update count less than30.py

--- count less than30.py
+++ count less than30.py
@@ -17,4 +17,28 @@ fun(n)
 output
 
 {32, 34, 37, 41, 17, 21, 22, 26, 29}
-Number of elements less than 30 are:  5
+
+
+
+
+
+import random
+n={random.randint(15,45) for i in range (10)}
+print(n)
+c=0
+for x in n:
+    if x<30:
+        c=c+1
+print('numbers less than 30 are',c)
+
+new=set(filter(lambda x:x<35,n))
+print('set having less than 35:',new)
+
+
+output
+
+{35, 36, 37, 39, 45, 15, 17, 28}
+numbers less than 30 are 3
+set having less than 35: {17, 28, 15}
+
+=== Code Execution Successful ===
